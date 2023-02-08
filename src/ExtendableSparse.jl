9569e48850c97798827baa42b513c5da185fe39a
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2023-02-09 00:19:17 +0100

    Let ExtendableSparseMatrix be an AbstractSparseMatrixCSC

--- src/ExtendableSparse.jl
+++ src/ExtendableSparse.jl
@@ -16,7 +16,7 @@ using Requires
 
 using DocStringExtensions
 
-import SparseArrays: rowvals, getcolptr, nonzeros
+import SparseArrays: AbstractSparseMatrixCSC,  rowvals, getcolptr, nonzeros
 
 include("sparsematrixcsc.jl")
 include("sparsematrixlnk.jl")
--- src/linearsolve.jl
+++ src/linearsolve.jl
@@ -3,15 +3,15 @@
 
 Create LinearProblem from ExtendableSparseMatrix.
 """
-function LinearSolve.LinearProblem(A::ExtendableSparseMatrix, b::AbstractArray)
-    LinearSolve.LinearProblem(sparse(A), b)
-end
+# function LinearSolve.LinearProblem(A::ExtendableSparseMatrix, b::AbstractArray)
+#     LinearSolve.LinearProblem(sparse(A), b)
+# end
 
 """
     $(SIGNATURES)
 
 Update LinearSolve cache.
 """
-function LinearSolve.set_A(cache::LinearSolve.LinearCache, A::ExtendableSparseMatrix)
-    LinearSolve.set_A(cache, sparse(A))
-end
+# function LinearSolve.set_A(cache::LinearSolve.LinearCache, A::ExtendableSparseMatrix)
+#     LinearSolve.set_A(cache, sparse(A))
+# end
