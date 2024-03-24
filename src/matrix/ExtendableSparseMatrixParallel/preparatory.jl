0f282c6f37b45146816a0243e5f6ff7f24e2d84e
Johannes Taraz <johannes.taraz@gmail.com>
2024-03-24 17:40:05 +0100

    added preparation for edgewise assembly

--- src/matrix/ExtendableSparseMatrixParallel/preparatory.jl
+++ src/matrix/ExtendableSparseMatrixParallel/preparatory.jl
@@ -6,24 +6,31 @@
 `depth` is the number of partition layers, for depth=1, there are nt parts and 1 separator, for depth=2, the separator is partitioned again, leading to 2*nt+1 submatrices...
 To assemble the system matrix parallely, things such as `cellsforpart` (= which thread takes which cells) need to be computed in advance. This is done here.
 """
-function preparatory_multi_ps_less_reverse(nm, nt, depth, Ti; sequential=false, x0=0.0, x1=1.0, minsize_sepa=10, do_print=false, check_partition=false)
+function preparatory_multi_ps_less_reverse(nm, nt, depth, Ti; sequential=false, assembly=:cellwise, x0=0.0, x1=1.0, minsize_sepa=10, do_print=false, check_partition=false)
 	grid = getgrid(nm; x0, x1)
 	adepth = 0
 	if sequential
-		(allcells, start, cellparts, adepth) = grid_to_graph_ps_multi!(grid, nt, depth; minsize_sepa, do_print)#)
+		(allcells, start, cellparts, adepth) = grid_to_graph_cellwise!(grid, nt, depth; minsize_sepa, do_print)#)
 	else
-		(allcells, start, cellparts, adepth) = grid_to_graph_ps_multi_par!(grid, nt, depth; minsize_sepa, do_print)
+		(allcells, start, cellparts, adepth) = grid_to_graph_cellwise_par!(grid, nt, depth; minsize_sepa, do_print)
 	end
 
 	if (adepth != depth) && do_print
 		@info "The requested depth of partitioning is too high. The depth is set to $adepth."
 	end
-
 	depth = adepth
-	cfp = bettercellsforpart(cellparts, depth*nt+1)
+	
+	if assembly == :cellwise
+		cfp = bettercellsforpart(cellparts, depth*nt+1)
 
+	else
+		edgeparts = edgewise_partition_from_cellwise_partition(grid, cellparts)
+		cfp = bettercellsforpart(edgeparts, depth*nt+1)
+	end
+
+		
 	if check_partition
-		validate_partition(grid, cellparts, start, allcells, nt, depth)
+		validate_partition(grid, cellparts, start, allcells, nt, depth, assembly)
 	end
 
 	#@info length.(cfp)
@@ -126,7 +133,7 @@ function get_nnnts_and_sortednodesperthread_and_noderegs_from_cellregs_ps_less_r
 		tmpctr = 1
 		for cr in sortedcellregs
 			crmod = (cr-1)%nt+1
-			level = Int(ceil(cr/nt))
+			#level = Int(ceil(cr/nt))
 			if !(crmod in tmp[1:tmpctr-1])
 				gictrs[crmod] += 1 # , level] += 1
 				sortednodesperthread[crmod,nj] = gictrs[crmod]
@@ -235,7 +242,7 @@ The function assigns colors/partitons to each cell in the `grid`. First, the gri
 `nt` is the number of threads.
 `depth` is the number of partition layers, for depth=1, there are nt parts and 1 separator, for depth=2, the separator is partitioned again, leading to 2*nt+1 submatrices...
 """
-function grid_to_graph_ps_multi!(grid, nt, depth; minsize_sepa=10, do_print=false)
+function grid_to_graph_cellwise!(grid, nt, depth; minsize_sepa=10, do_print=false)
 	A = SparseMatrixLNK{Int64, Int64}(num_cells(grid), num_cells(grid))
 	number_cells_per_node = zeros(Int64, num_nodes(grid))
 	for j=1:num_cells(grid)
@@ -304,11 +311,10 @@ function grid_to_graph_ps_multi!(grid, nt, depth; minsize_sepa=10, do_print=fals
 		end
 	end
         
-    return allcells, start, cellregs, actual_depth, ACSC
+    return allcells, start, cellregs, actual_depth
 end
 
-
-function grid_to_graph_ps_multi_par!(grid, nt, depth; minsize_sepa=10, do_print=false)
+function grid_to_graph_cellwise_par!(grid, nt, depth; minsize_sepa=10, do_print=false)
 	As = [ExtendableSparseMatrix{Int64, Int64}(num_cells(grid), num_cells(grid)) for tid=1:nt]
 	number_cells_per_node = zeros(Int64, num_nodes(grid))
 	
@@ -395,6 +401,195 @@ function grid_to_graph_ps_multi_par!(grid, nt, depth; minsize_sepa=10, do_print=
     #grid[CellRegions] = cellregs
     #grid
     return allcells, start, cellregs, actual_depth
+end
+
+function grid_to_graph_edgewise!(grid, nt, depth; minsize_sepa=10, do_print=false)
+	ce = grid[CellEdges]
+    A = SparseMatrixLNK{Int64, Int64}(num_edges(grid), num_edges(grid))
+	number_edges_per_node = zeros(Int64, num_nodes(grid))
+	
+    for i=1:num_edges(grid)
+        for node_id in grid[EdgeNodes][:,i] 
+            number_edges_per_node[node_id] += 1
+        end
+    end
+
+    alledges = zeros(Int64, sum(number_edges_per_node))
+	start = ones(Int64, num_nodes(grid)+1)
+	start[2:end] += cumsum(number_edges_per_node)
+	number_edges_per_node .= 0
+	
+    for j=1:num_edges(grid)
+		for node_id in grid[EdgeNodes][:,j]
+			alledges[start[node_id] + number_edges_per_node[node_id]] = j
+			number_edges_per_node[node_id] += 1
+		end
+	end
+
+	for j=1:num_nodes(grid)
+		edges = @view alledges[start[j]:start[j+1]-1]
+		for (i,id1) in enumerate(edges)
+			for id2 in edges[i+1:end]
+				A[id1,id2] = 1
+				A[id2,id1] = 1
+			end
+		end	
+	end
+
+	ACSC = SparseArrays.SparseMatrixCSC(A)
+	
+	partition = Metis.partition(ACSC, nt)
+
+    sn = Vector{Int64}(undef, num_edges(grid))
+	gi = Vector{Int64}(undef, num_edges(grid))
+	ctr_sepanodes = 0
+    
+	edgeregs  = copy(partition)
+    for tid=1:nt
+        for j=1:num_edges(grid)
+            if edgeregs[j] == tid
+                rows = vcat(ACSC.rowval[ACSC.colptr[j]:(ACSC.colptr[j+1]-1)], [j])
+                if how_many_different_below(edgeregs[rows], nt+1) > 1 
+                    edgeregs[j] = nt+1 #+ctr_sepanodes
+                    ctr_sepanodes += 1
+                    sn[ctr_sepanodes] = j
+                    gi[ctr_sepanodes] = j
+                end
+            end
+        end
+	end
+
+    sn = sn[1:ctr_sepanodes]
+    gi = gi[1:ctr_sepanodes]
+    
+    if do_print
+        @info "At level $(1), we found $ctr_sepanodes cells that have to be treated in the next iteration!"
+    end
+    
+    RART = copy(ACSC)
+    actual_depth = 1
+	for level=1:depth-1
+		RART, ctr_sepanodes, sn, gi = separate!(edgeregs, num_edges(grid), RART, nt, level, ctr_sepanodes, sn, gi, do_print)
+        actual_depth += 1
+		if ctr_sepanodes < minsize_sepa
+			break
+		end
+	end
+        
+    return alledges, start, edgeregs, actual_depth
+end
+
+function grid_to_graph_edgewise_par!(grid, nt, depth; minsize_sepa=10, do_print=false)
+	ce = grid[CellEdges]
+    cn = grid[EdgeNodes]
+	
+    As = [ExtendableSparseMatrix{Int64, Int64}(num_edges(grid), num_edges(grid)) for tid=1:nt]
+	number_edges_per_node = zeros(Int64, num_nodes(grid))
+	
+	
+	for j=1:num_edges(grid)
+		tmp = view(cn, :, j)
+		for node_id in tmp
+			number_edges_per_node[node_id] += 1
+		end
+	end
+		
+	
+	alledges = zeros(Int64, sum(number_edges_per_node))
+	start = ones(Int64, num_nodes(grid)+1)
+	start[2:end] += cumsum(number_edges_per_node)
+	number_edges_per_node .= 0
+	
+	for j=1:num_edges(grid)
+		tmp = view(cn, :, j)
+		for node_id in tmp
+			alledges[start[node_id] + number_edges_per_node[node_id]] = j
+			number_edges_per_node[node_id] += 1
+		end
+	end
+
+	node_range = get_starts(num_nodes(grid), nt)
+	Threads.@threads for tid=1:nt
+		for j in node_range[tid]:node_range[tid+1]-1
+			edges = @view alledges[start[j]:start[j+1]-1]
+			l = length(edges)
+			for (i,id1) in enumerate(edges)
+				ce = view(edges, i+1:l)
+				for id2 in ce
+					As[tid][id1,id2] = 1
+					As[tid][id2,id1] = 1
+					
+				end
+			end	
+		end
+		ExtendableSparse.flush!(As[tid])
+	end
+	
+	ACSC = add_all_par!(As).cscmatrix
+	
+	cellregs = Metis.partition(ACSC, nt)
+	
+	sn = [Vector{Int64}(undef, Int(ceil(num_cells(grid)/nt))) for tid=1:nt]
+	ctr_sepanodess = zeros(Int64, nt)
+    
+    @threads for tid=1:nt
+        for j=1:num_edges(grid)
+            if cellregs[j] == tid
+                rows = vcat(ACSC.rowval[ACSC.colptr[j]:(ACSC.colptr[j+1]-1)], [j])
+                if how_many_different_below(cellregs[rows], nt+1) > 1 
+                    cellregs[j] = nt+1 #+ctr_sepanodes
+                    ctr_sepanodess[tid] += 1
+                    sn[tid][ctr_sepanodess[tid]] = j
+                end
+            end
+        end
+	end
+
+    for tid=1:nt
+        sn[tid] = sn[tid][1:ctr_sepanodess[tid]]
+    end
+    ctr_sepanodes = sum(ctr_sepanodess)
+    sn = vcat(sn...)
+    gi = copy(sn)
+
+    if do_print
+        @info "At level $(1), we found $ctr_sepanodes edges that have to be treated in the next iteration!"
+    end
+
+    RART = ACSC
+    actual_depth = 1
+	for level=1:depth-1
+		RART, ctr_sepanodes, sn, gi = separate!(cellregs, num_cells(grid), RART, nt, level, ctr_sepanodes, sn, gi, do_print)
+        actual_depth += 1
+		if ctr_sepanodes < minsize_sepa
+			break
+		end
+	end
+    
+    #grid[CellRegions] = cellregs
+    #grid
+    return alledges, start, cellregs, actual_depth
+end
+
+
+function edgewise_partition_from_cellwise_partition(grid, cellregs)
+	ce = grid[CellEdges]
+	if num_edges(grid) == 0
+		grid[EdgeNodes]
+	end
+	
+	edgeregs = maximum(cellregs)*ones(Int64, num_edges(grid))
+
+	for icell=1:num_cells(grid)
+		tmp = cellregs[icell]
+		for iedge in ce[:,icell]
+			if tmp < edgeregs[iedge]
+				edgeregs[iedge] = tmp
+			end
+		end
+	end
+
+	edgeregs
 end
 
 """
@@ -636,9 +831,14 @@ function check_partition(nm, nt, depth)
 end
 =#
 
-function validate_partition(grid, cellregs, start, allcells, nt, depth)
-	@info "Node based validation"
+function validate_partition(grid, cellregs, start, allcells, nt, depth, assemblytype)
 	violation_ctr = 0
+
+	if assemblytype == :cellwise
+		key = CellNodes
+	else
+		key = EdgeNodes
+	end
 
 	for j=1:num_nodes(grid)
 		cells = @view allcells[start[j]:start[j+1]-1]
@@ -651,14 +851,14 @@ function validate_partition(grid, cellregs, start, allcells, nt, depth)
 				violation_ctr += 1
 
 				if violation_ctr == 1
-					@info "Node Id : ", j
-					@info "Cellregs: ", sortedcellregs
-					@info "Levels  : ", levels
+					@info "Node Id : $j (we only show one violation)"
+					@info "Cellregs: $sortedcellregs"
+					@info "Levels  : $levels"
 					
 					loc = findall(x->x==4, Int.(ceil.(cellregs[allcells[start[j]:start[j+1]-1]]/nt)))
 					cells_at_level4 = allcells[loc.+(start[j]-1)]
 					@info cells_at_level4, cellregs[cells_at_level4]
-					@info grid[CellNodes][:,cells_at_level4[1]], grid[CellNodes][:,cells_at_level4[2]]
+					@info grid[key][:,cells_at_level4[1]], grid[key][:,cells_at_level4[2]]
 				end
 			end
 		end
